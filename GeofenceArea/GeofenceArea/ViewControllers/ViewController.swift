8341b23b856279a942b1254265c1a3f4b7143f9d
= <=>
2021-06-18 09:41:06 +0700

    Create base project

---
+++ GeofenceArea/GeofenceArea/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeofenceArea
+//
+//  Created by Nguyen Tan Dung on 18/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
